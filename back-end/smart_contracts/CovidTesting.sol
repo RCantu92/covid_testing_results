52a5f3b6271cc0daa184239b480ea35b2811b8e5
Roberto Cantu <robertocantu@Robertos-MacBook-Pro.local>
2020-06-12 15:44:54 -0500

    Update comments

--- back-end/smart_contracts/CovidTesting.sol
+++ back-end/smart_contracts/CovidTesting.sol
@@ -8,22 +8,20 @@ contract CovidTesting {
     address contractOwner;
     
     // Set the msg.sender of the contract
-    // to ass the address that owns the
-    // contract.
+    // as the address that owns the contract.
     constructor() public {
         contractOwner = msg.sender;
     }
     
-    // Struct storing a heatlh professionals
-    // ethereum address, license number,
-    // and if they are qualified to administer
-    // COVID tests.
+    // Struct storing a heatlh professional's
+    // ethereum address and if they are
+    // qualified to administer COVID tests.
     struct HealthcareProf {
         address ethAddr;
         bool covidQualified;
     }
     
-    // Struct that storess a civillians ethereum
+    // Struct that storess a civillian;s ethereum
     // address, whether they've been tested for
     // COVID, what time the test was done, and
     // who administered the test.
@@ -40,7 +38,7 @@ contract CovidTesting {
     mapping(uint => HealthcareProf) healthcareProfById;
     
     // Mapping that allows you to input a civillian's
-    // government issued id number to pull up their
+    // government issued ID number to pull up their
     // respective struct.
     mapping(uint => Civillian) civillianByGovntId;
     
@@ -52,23 +50,23 @@ contract CovidTesting {
     }
     
     // Modifier that verifies person calling the function
-    // is COVID qualified.
+    // is a healthcare professional and COVID qualified.
     modifier onlyHealthcareProf(uint _licenseNum) {
         require(msg.sender == healthcareProfById[_licenseNum].ethAddr, "You are not a qualified healthcare professional.");
         require(healthcareProfById[_licenseNum].covidQualified == true, "You are not a qualified healthcare professional.");
         _;
     }
     
-    // Function that takes the civillians
-    // government id number as a argument
-    // to then add show they have been tested.
+    // Function that takes a healthcare professional's
+    // license number as a argument to add them and show
+    // they are qualified to administer COVID tests.
     function addHealthcareProfessional(address _healthcareProfAddr, uint _licenseNum) public onlyContractOwner {
         healthcareProfById[_licenseNum] = HealthcareProf(_healthcareProfAddr, true);
     }
     
-    // Function that takes the civillians
-    // government id number as a argument
-    // to then add show they have been tested.
+    // Function that takes the civillian's government
+    // ID number as a argument to add them and then
+    // show they have been tested.
     function addTestedCivillian(uint _govntId, uint _healthcareProflicenseNum, address _civillianEthAddr) public onlyHealthcareProf(_healthcareProflicenseNum){
         civillianByGovntId[_govntId] = Civillian(_civillianEthAddr, true, now, _healthcareProflicenseNum);
     }
@@ -76,7 +74,7 @@ contract CovidTesting {
     // Function that would check to verify
     // that a civillian has been tested for COVID.
     // Returns whether they have been tested, time
-    // of test, relative to block time, and the
+    // of test (relative to block time)  and the
     // license number of the healthcare professional
     // who administered the test.
     function isCivillianTested(uint _govntId) public view returns(bool, uint, uint) {
