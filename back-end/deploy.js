55fce3566a5a833fb5ca97498cb6770d9a17a6e4
Roberto Cantu <robertocantu@Robertos-MacBook-Pro.local>
2020-06-17 18:14:43 -0500

    Deploy to ropsten testnet

---
+++ back-end/deploy.js
@@ -0,0 +1,55 @@
+// Load in the hdwallet module
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+// Load in the web3 module
+const Web3 = require('web3');
+// Load in compiled COVID Testing
+// contract from JSON file.
+const compiledCovidTesting = require('./build/CovidTesting.json');
+
+/**
+ * @dev Declaring a new variable instance of HDWalletProvider.
+ * @param 'river tag balcony picture boil kind drop pig polar save poem time'
+ * which account to use to provide the ether. (NOTE: SAVE THE SEED UNDER A HIDDEN FILE!!!)
+ * @param 'https://ropsten.infura.io/v3/491ffd9a994941089a5e348aba1bb061'
+ * Specifies which outside node we are going to connect to. (Ethereum-Ropsten)
+ */
+const provider = new HDWalletProvider(
+    'river tag balcony picture boil kind drop pig polar save poem time',
+    'https://ropsten.infura.io/v3/491ffd9a994941089a5e348aba1bb061'
+);
+
+/**
+ * @dev Declaring a new variable instance of web3.
+ * @param provider the account used to fund and the connecting node.
+ */
+const web3 = new Web3(provider);
+
+/**
+ * @dev A function that deploys to the ropsten Ethereum network.
+ */
+const deploy = async () => {
+    // List of all the accounts that have been unlocked by HDWalletProvider.
+    const accounts = await web3.eth.getAccounts();
+
+    // Logging the first derived account.
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    /**
+     * @dev Saving the instance of the deployed contract.
+     * @param compiledCovidTesting.interface ABI from compiled
+     * COVID Testing contract. Used JSON.parse because
+     * Contract() requires JavaScript object.
+     * @param data compiled contract's bytecode.
+     * @param gas fee paid to the network to run code. Counted in wei.
+     * @param from account from which gas is being paid from.
+     */
+    const result = await new web3.eth.Contract(JSON.parse(compiledCovidTesting.interface))
+        .deploy({ data: compiledCovidTesting.bytecode })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    // Logging the address of the deployed contract.
+    console.log('Contract deployed to', result.options.address);
+};
+
+// Calling the recently declared function.
+deploy();
--- test/CovidTesting.test.js
+++ test/CovidTesting.test.js
@@ -1,8 +1,5 @@
-// Load in assert module
 const assert = require('assert');
-// Load in Ganache CLI
 const ganache = require('ganache-cli');
-// Load in web3
 const Web3 = require('web3');
 // Create an instance of web3 that connects
 // to the ganache local blockchain 
@@ -13,11 +10,12 @@ const web3 = new Web3(ganache.provider());
 // from it's JSON file.
 const compiledCovidTesting = require('../back-end/build/CovidTesting.json');
 
-// Declaring a variable with no value.
+// Declaring two variables with no value.
 let accounts;
 let covidTesting;
 
-beforeEach(async () => {
+// Set up before running any test.
+beforeEach( async () => {
     // Get a list of generated accounts and
     // save to previously declared variable.
     accounts = await web3.eth.getAccounts();
@@ -34,34 +32,52 @@ beforeEach(async () => {
     covidTesting = await new web3.eth.Contract(JSON.parse(compiledCovidTesting.interface))
         .deploy({ data: compiledCovidTesting.bytecode })
         .send({ from: accounts[0], gas: '1000000' });
-
-    await covidTesting.methods.addHealthcareProfessional(accounts[1], 123456).send({ 
-        from: accounts[0]
-    });
 });
 
 describe('COVID Testing', () => {
     // Confirms that the contract can be deployed.
-    it('Deploys a factory and a campaign.', () => {
+    it('Deploys a COVID Testing contract', async () => {
         assert.ok(covidTesting.options.address);
     });
 
     // Confirms it can save address as the owner of the contract.
-    it('Marks caller as the owner of the contract.', async () => {
+    it('Verifies caller as the owner of the contract.', async () => {
         const contractOwner = await covidTesting.methods.contractOwner().call();
         assert.equal(accounts[0], contractOwner);
     });
 
-    // Confirms you can add healthcare professionals that
-    // can administer tests.
-    it('Allows for the addition of qualified healthcare professionals.', async () => {
+    // Confirms qualified healthcare professionals
+    // can be added.
+    it('Confirms qualified healthcare professional.', async () => {
+        await covidTesting.methods.addHealthcareProfessional(accounts[1], 123456).send({ 
+            from: accounts[0]
+        });
         const isHealthcareProfQualified = await covidTesting.methods.isHealthcareProfQualified(123456).call();
         assert(isHealthcareProfQualified);
     });
 
-    // Confirms you can add civillians that
-    // have had the test administered.
+    // Confirms only owner of contract can add
+    // qualified healthcare professionals.
+    it('Verifies only contract owner can add healthcare professionals.', async () => {
+        try {
+            await covidTesting.methods.addHealthcareProfessional(accounts[1], 123456).send({ 
+                from: accounts[3]
+            });
+            assert(false);
+        } catch (err) {
+            assert(err);
+        }
+    });
+
+    // End-to-end test to confirm the request
+    // process works as a whole.
     it('Allows for the addition of a tested civillian.', async () => {
+        await covidTesting.methods.addHealthcareProfessional(accounts[1], 123456).send({ 
+            from: accounts[0]
+        });
+        const isHealthcareProfQualified = await covidTesting.methods.isHealthcareProfQualified(123456).call();
+        assert(isHealthcareProfQualified);
+
         await covidTesting.methods.addTestedCivillian(33448863, 123456, accounts[2]).send({
             from: accounts[1]
         });
