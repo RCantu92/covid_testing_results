c851a4593a55037bc593c25a42b93c0b639808aa
Roberto Cantu <robertocantu@Robertos-MacBook-Pro.local>
2020-06-25 15:29:13 -0500

    Update index.js to include search bar

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "924e344589da97410be6";
+/******/ 	var hotCurrentHash = "1d5d9007ac116bf95edc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import instance from '../back-end/covidTesting.js';
+import { Card, Form, Button } from 'semantic-ui-react';
+import covidTesting from '../back-end/covidTesting.js';
 
 class CovidTesting extends Component {
-    async componentDidMount() {
-        const campaigns = await instance.methods.getDeployedCampaigns().call();
+    static async getInitialProps() {
+        const contractOwner = await covidTesting.methods.contractOwner().call();
+        
+        return { contractOwner };
+    }
 
-        console.log(campaigns);
+    renderContractOwner() {
+        const items = [{
+            header: 'Contract Owner Address:',
+            description: 'Ethereum address of the owner of the contract.',
+            meta: this.props.contractOwner,
+            fluid: true
+        }];
+
+        return <Card.Group items={items} />;
     }
 
     render() {
-        return <div>
-            Covid Testing Index!
-        </div>
-    }
-}
+        return(
+            <div>
+                <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+                {this.renderContractOwner()}
+
+                <Form>
+                    <Form.Field>
+                        <label>Confirm if civllian has been tested for COVID-19</label>
+                        <input placeholder="Search by Government Issued ID"/>
+                    </Form.Field>
+
+                    <Form.Field>
+                        <label>Confirm if healthcare professional is qualified to administer COVID-19 tests</label>
+                        <input placeholder="Search by Healthcare Professional License Number"/>
+                    </Form.Field>
+
+                    <Button>Search</Button>
+                </Form>
+            </div>
+        )
+    };
+};
 
 export default CovidTesting;
